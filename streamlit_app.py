22f5ce363c9e0aa074ac14c4fea409dfb40dd41d
Jenn1ferrsfriedplaintain <jennifer.onyekwere@scholar.smash.org>
2025-07-18 18:17:31 +0000

    Adding social media file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
+@st.cache_data
+def load_data():
+    return pd.read_csv("/kaggle/input/social-media-addiction-vs-relationships/Students Social Media Addiction.csv")
+
+
+st.title("Students'Addiction Data")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Description: This data set displays the addiction rates and details of students throughout the United States"
 )
+
